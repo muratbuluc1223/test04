1b59fd0ed2006bf6345c2a73c4b980d3ee0be02b
muratbuluc1223 <muratbuluc1223@outlook.com>
2023-08-26 19:56:41 +0300

    test1

---
+++ src/main/java/github022/test4.java
@@ -0,0 +1,10 @@
+package github022;
+
+public class test4 {
+    public static void main(String[] args) {
+        //version 1
+        System.out.println("test1");
+        //version2
+        System.out.println("test2");
+    }
+}
